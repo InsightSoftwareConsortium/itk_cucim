a2cf2fb37152e6999a09a9b6f577df30bc4bc8de
Gregory Lee <grlee77@gmail.com>
2022-05-03 20:36:41 -0400

    apply black

--- test/filtering/test_smoothing.py
+++ test/filtering/test_smoothing.py
@@ -7,13 +7,12 @@ import pytest
 from itk_cucim.filtering import smoothing
 
 
-class TestSmoothing():
-
+class TestSmoothing:
     def setup_class(self):
-        data = Path(__file__).absolute().parent.parent / 'input' / 'head_mr.mha'
+        data = Path(__file__).absolute().parent.parent / "input" / "head_mr.mha"
         self.image = itk.imread(data)
 
-    @pytest.mark.parametrize('radius', [1, (3, 2, 1)])
+    @pytest.mark.parametrize("radius", [1, (3, 2, 1)])
     def test_median_image_filter(self, radius):
 
         median_ref = itk.median_image_filter(self.image, radius=radius)
