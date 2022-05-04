a611c7ff291b02f3ec12f7bae6d2bfb9327ad220
Gregory Lee <grlee77@gmail.com>
2022-05-03 20:28:23 -0400

    parameterize tests
    use setup_class to avoid repeatedly loading the test data

--- test/filtering/test_smoothing.py
+++ test/filtering/test_smoothing.py
@@ -1,20 +1,25 @@
-from itk_cucim.filtering import smoothing
-import itk
-import numpy as np
-
 from pathlib import Path
 
-def test_median_image_filter():
-    image = itk.imread(Path(__file__).absolute().parent.parent / 'input' / 'head_mr.mha')
+import itk
+import numpy as np
+import pytest
 
-    median_ref = itk.median_image_filter(image, radius=[1,2,1])
-    median_cucim = smoothing.cucim_median_image_filter(image, radius=[1,2,1])
+from itk_cucim.filtering import smoothing
 
-    comparison = itk.comparison_image_filter(median_ref, median_cucim, verify_input_information=True)
-    assert np.sum(comparison) == 0.0
 
-    median_ref = itk.median_image_filter(image, radius=[3,2,1])
-    median_cucim = smoothing.cucim_median_image_filter(image, radius=[3,2,1])
+class TestSmoothing():
 
-    comparison = itk.comparison_image_filter(median_ref, median_cucim, verify_input_information=True)
-    assert np.sum(comparison) == 0.0
+    def setup_class(self):
+        data = Path(__file__).absolute().parent.parent / 'input' / 'head_mr.mha'
+        self.image = itk.imread(data)
+
+    @pytest.mark.parametrize('radius', [(1, 2, 1), (3, 2, 1)])
+    def test_median_image_filter(self, radius):
+
+        median_ref = itk.median_image_filter(self.image, radius=radius)
+        median_cucim = smoothing.cucim_median_image_filter(self.image, radius=radius)
+
+        comparison = itk.comparison_image_filter(
+            median_ref, median_cucim, verify_input_information=True
+        )
+        assert np.sum(comparison) == 0.0
